de41d5a0fdfcdde1433418edded0dd4ff508c9a0
jmpfbmx <jmpf.bmx@gmail.com>
2021-08-21 12:32:33 +0200

    freezerhd: patches: apply: Fix a typo
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ic9d428c2a90f709f0c5351700f1cce71573d56b1

--- patches/apply.sh
+++ patches/apply.sh
@@ -4,7 +4,7 @@ cd frameworks/native/
 git apply -v ../../device/bq/freezerhd/patches/frameworks/native/0001-Restore-HW-rotation-properties.patch
 cd ../..
 cd packages/apps/FMRadio
-git apply -v ../../device/bq/freezerhd/patches/packages/apps/FMRadio/0001-Suppress-compiler-warnings.patch
+git apply -v ../../../device/bq/freezerhd/patches/packages/apps/FMRadio/0001-Suppress-compiler-warnings.patch
 cd ../../..
 cd vendor/lineage
 git apply -v ../../device/bq/freezerhd/patches/vendor/lineage/0001-kernel-Don-t-use-build-image-kernel-modules-instead-.patch.patch
